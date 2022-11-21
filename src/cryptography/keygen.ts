3aa25ec8603e19692885808d58314414926e9cae
Ilya Zaytsev <ilya@zaytsev.xyz>
2022-11-21 23:53:22 +0300

    keygen mnemonic phrase

--- src/cryptography/keygen.ts
+++ src/cryptography/keygen.ts
@@ -1,5 +1,6 @@
 import { ec } from 'elliptic'
 import { getAddressFromPublicKey } from './utils'
+import { entropyToMnemonic } from 'bip39'
 
 const elliptic: ec = new ec('secp256k1')
 
@@ -8,8 +9,11 @@ const keyPair: ec.KeyPair = elliptic.genKeyPair()
 const publicKey: string = keyPair.getPublic('hex')
 const privateKey: string = keyPair.getPrivate('hex')
 
+const mnemonic: string = entropyToMnemonic(privateKey)
+
 const address: string = getAddressFromPublicKey(publicKey)
 
 console.log('Private Key:', privateKey)
+console.log('Mnemonic:', mnemonic)
 console.log('Public Key:', publicKey)
 console.log('Address:', address)
