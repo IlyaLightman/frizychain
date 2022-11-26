923de630d154bf99939c20f58525867d6e6a885f
Ilya Zaytsev <ilya@zaytsev.xyz>
2022-11-27 02:40:58 +0300

    transactions sign ways, separate getHash

--- README.md
+++ README.md
@@ -6,4 +6,12 @@ Simple Bitcoin-like blockchain
 
 To generate private/public keys pair, mnemonic phrase and an address, use `npm run keygen`
 
-Similar to Bitcoin it uses **ECDSA** eleptic-curve cryptography algorithm for generating and checking keys and **Base58check** encoding for addresses, which are just public keys hashed with **SHA256** and then with **RIPEMD160**
+Similar to Bitcoin it uses **ECDSA** eleptic-curve cryptography algorithm for generating and checking keys and **Base58check** encoding for addresses, which are just public keys hashed with **SHA256** and then with **RIPEMD160**.
+
+### Runtime
+
+At first it will only support **Pay to Public Key Hash** (until solving Transaction malleability). Although you are able to fill transaction scripts whatever you want, if it isn't **P2PHK**, it will not be safety.
+
+Syntax similar to Bitcoin:
+
+![](http://orm-chimera-prod.s3.amazonaws.com/1234000001802/images/msbt_0501.png)
--- src/blockchain/transaction/index.ts
+++ src/blockchain/transaction/index.ts
@@ -1,15 +1,41 @@
+import { ec } from 'elliptic'
+import { getCommonHash } from '../../cryptography/utils'
 import TransactionInput from './input'
 import TransactionOutput from './output'
 
 export default class Transaction {
-    inputs: TransactionInput[]
-    outputs: TransactionOutput[]
-    hash: string
+	inputs: TransactionInput[]
+	outputs: TransactionOutput[]
+	signature: ec.Signature | null
 
-    constructor(inputs: TransactionInput[], outputs: TransactionOutput[]) {
-        this.inputs = inputs
-        this.outputs = outputs
-        this.hash = inputs.reduce((hash, input) => hash + input.hash, '') +
-            outputs.reduce((hash, input) => hash + input.hash, '')
-    }
+	constructor(inputs: TransactionInput[], outputs: TransactionOutput[]) {
+		this.inputs = inputs
+		this.outputs = outputs
+		this.signature = null
+	}
+
+	public getHash: () => string = () => {
+		return getCommonHash(
+			this.inputs.reduce((hash, input) => hash + input.hash, '') +
+				this.outputs.reduce((hash, input) => hash + input.hash, '')
+		)
+	}
+
+	// Temporary addition way to sign, i don't know why
+
+	public sign: (keyPair: ec.KeyPair) => void = keyPair => {
+		const signatureObject = keyPair.sign(this.getHash())
+
+		this.signature = signatureObject
+	}
+
+	public verify: (publicKey: string) => boolean = publicKey => {
+		if (!this.signature) return false
+
+		const elliptic: ec = new ec('secp256k1')
+		const key = elliptic.keyFromPublic(publicKey, 'hex')
+		const verified = key.verify(this.getHash(), this.signature)
+
+		return verified
+	}
 }
